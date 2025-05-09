2067bf9c03bd2e6ed52ed3731013eb58a88ab69c
MatteoBucc <mbuccolieri22@gmail.com>
2025-05-09 21:33:04 +0200

    aggiunta predict di un modello di huggingface

--- src/predict.py
+++ src/predict.py
@@ -3,19 +3,23 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from config import DEVICE, MODEL_NAME
+from config import DEVICE
 import argparse
-
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from model import get_model  # Se hai un metodo per costruire il tuo modello
+import os
 
 def predict(sentence1: str, sentence2: str, model_path: str):
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)  # Usa il modello di base per il tokenizer
+    # Usa model_path anche come base per il tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
 
-    # Carica il modello con i pesi salvati nel file .pth
-    model = get_model()  # O costruisci il modello in base alla tua architettura
-    model.load_state_dict(torch.load(model_path))  # Carica i pesi dal file .pth
+    if os.path.isdir(model_path):  # modello Hugging Face scaricato localmente
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
+    elif model_path.endswith(".pth"):  # modello locale in PyTorch (.pth)
+        from model import get_model  # Assumi che get_model() ritorni lo stesso architettura
+        model = get_model()
+        model.load_state_dict(torch.load(model_path, map_location=DEVICE))
+    else:  # modello Hugging Face da Internet (es. "utente/modello")
+        model = AutoModelForSequenceClassification.from_pretrained(model_path)
+
     model.to(DEVICE)
     model.eval()
 
@@ -41,7 +45,7 @@ def main():
     parser = argparse.ArgumentParser(description="Paraphrase prediction")
     parser.add_argument("sentence1", type=str, help="Prima frase")
     parser.add_argument("sentence2", type=str, help="Seconda frase")
-    parser.add_argument("--model_path", type=str, required=True, help="Path al modello fine-tunato")
+    parser.add_argument("--model_path", type=str, required=True, help="Path al modello fine-tunato o nome Hugging Face")
     args = parser.parse_args()
 
     pred, conf = predict(args.sentence1, args.sentence2, args.model_path)
