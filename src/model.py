21ac00baa3689c8189e1c34fbed1a0b185191bf6
MatteoBucc <mbuccolieri22@gmail.com>
2025-05-15 16:54:05 +0200

    fix

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ import torch
 def get_model():
     model = AutoModelForSequenceClassification.from_pretrained(
         MODEL_NAME,
-        num_labels=2
+        num_labels=2 
     )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     return model.to(device)
