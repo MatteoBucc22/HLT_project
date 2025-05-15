b75540d82a14fe77869b342aa8defb661356e13f
Agnese <a.bertoncini1@studenti.unipi.it>
2025-05-15 16:58:55 +0200

    fix

--- src/model.py
+++ src/model.py
@@ -1,12 +1,13 @@
-# src/model.py
 import os
-from sentence_transformers import SentenceTransformer
-from config import MODEL_NAME, DEVICE
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+from transformers import AutoModelForSequenceClassification
+from config import MODEL_NAME
+import torch
 
 def get_model():
     model = AutoModelForSequenceClassification.from_pretrained(
         MODEL_NAME,
-        num_labels=2 
+        num_labels=2
     )
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     return model.to(device)
